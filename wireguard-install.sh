8e8c2c0a6c7c45511f23fd62ec2d09ca7dfd1ab4
Hai Vo <truonghaiv@eggspot.app>
2023-08-05 12:42:22 +0700

    Test file

--- wireguard-install.sh
+++ wireguard-install.sh
@@ -325,12 +325,13 @@ function newClient() {
 
 		BASE_IP=$(echo "$SERVER_WG_IPV4" | awk -F '.' '{ print $1"."$2"."$3 }')
 		IPV4_EXISTS='0'
+		echo "Client WireGuard IPv4: ${BASE_IP}.${DOT_IP}"
+		
 		until [[ ${IPV4_EXISTS} == '0' ]]; do
 			# read -rp "Client WireGuard IPv4: ${BASE_IP}." -e -i "${DOT_IP}" DOT_IP
 			CLIENT_WG_IPV4="${BASE_IP}.${DOT_IP}"			
 			IPV4_EXISTS=$(grep -c "$CLIENT_WG_IPV4/32" "/etc/wireguard/${SERVER_WG_NIC}.conf")
-
-			echo "Client WireGuard IPv4: ${CLIENT_WG_IPV4}"
+			
 			if [[ ${IPV4_EXISTS} != 0 ]]; then
 				echo ""
 				echo -e "${ORANGE}A client with the specified IPv4 was already created, please choose another IPv4.${NC}"
